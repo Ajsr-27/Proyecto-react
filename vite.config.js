e22c3a076d2b45c2f9ce9cee8e9dc8365b36b96d
Ajsr-27 <augustossanchez@gmail.com>
2026-05-14 20:56:49 -0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ecomerce/',
+  base: '/Proyecto-react/',
   plugins: [react(), tailwindcss()],
 })
