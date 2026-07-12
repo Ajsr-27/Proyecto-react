60dd6a5e361b9a7d660eee8dc91f2db277ea048f
Ajsr-27 <augustossanchez@gmail.com>
2026-07-12 13:38:28 -0300

    Creacion de .env para ocultar los datos sensibles de la base de datos, actualizacion del git ingnore

--- src/Firebase/Config.js
+++ src/Firebase/Config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDetXNFbynE6a3oZqUrBTL8uNKXcnJUHSo",
-  authDomain: "neko-store-1b56b.firebaseapp.com",
-  projectId: "neko-store-1b56b",
-  storageBucket: "neko-store-1b56b.firebasestorage.app",
-  messagingSenderId: "760084517542",
-  appId: "1:760084517542:web:870898f8367330f5095a67",
-  measurementId: "G-02WMWR25MX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
